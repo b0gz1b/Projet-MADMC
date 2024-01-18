89d16dd79311549ac3966cf7cc72bb71052266f9
b0gz1b <28600665@ppti-gpu-1>
2024-01-18 08:58:05 +0100

    res expe comparaison

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import experiments as xp
 
 if __name__ == '__main__':
-	experiment1 = xp.Experiment(number_of_items = 20, 
-							 dimension = 3, 
+	experiment1 = xp.Experiment(number_of_items = 30, 
+							 dimension = 5, 
 							 file_path = "data/2KP200-TA-0.dat", 
 							 number_of_parameters_set = 20)
 
@@ -10,6 +10,6 @@ if __name__ == '__main__':
 	
 	# experiment1.plot_results_first_procedure(results, "../out/partie_1/")
  
-	results1, results2 = experiment1.run_exp_all_procedure(size_pop_init = 10, struct = "NDTree")
+	results1, results2 = experiment1.run_exp_all_procedure(size_pop_init = 100, struct = "NDTree")
 	
 	experiment1.plot_results(results1, results2, "out/comparaison/")
